77cf8795101a336d70b1868fa787a2fde5847286
AAAAAAAHSAN <bangladeshahsan@gmail.com>
2020-09-09 23:42:06 +0600

    Adding more authentication

--- app/Http/Controllers/ProfilesController.php
+++ app/Http/Controllers/ProfilesController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\User;
 use Illuminate\Http\Request;
+use Intervention\Image\Facades\Image;
 
 class ProfilesController extends Controller
 {
@@ -15,11 +16,15 @@ class ProfilesController extends Controller
 
     public function edit(User $user)
     {
+        $this->authorize('update',$user->profile);
+
         return view('profiles.edit',compact('user'));
     }
 
     public function update(User $user)
     {
+        $this->authorize('update',$user->profile);
+
         $data=request()->validate([
             'title'=>'required',
             'description'=>'required',
@@ -27,7 +32,19 @@ class ProfilesController extends Controller
             'image'=>'',
         ]);
 
-        $user->profile->update($data);
+        if(request('image')){
+            $imagepath = (request('image')->store('profile','public'));
+
+            $image = Image::make(public_path("storage/{$imagepath}"))->fit(1000,1000);
+            $image->save();
+        }
+
+
+        auth()->user()->profile->update(array_merge(
+            $data,
+            ['image'=>$imagepath]
+        ));
+
         return redirect("/profile/{$user->id}");
     }
 }
--- resources/views/profiles/index.blade.php
+++ resources/views/profiles/index.blade.php
@@ -4,14 +4,21 @@
 <div class="container">
     <div class="row">
         <div class="col-3 p-5">
-            <img src="/images/0019.jpg" height="90px" class="rounded-circle">
+            <img src="/storage/{{$user->profile->image}}"  class="w-100 rounded-circle">
         </div>
         <div class="col-9">
             <div class="d-flex justify-content-between align-items-baseline">
                 <h1>{{$user->username}}</h1>
-                <a href="/p/create">Add new post</a>
+
+                @can('update',$user->profile)
+                    <a href="/p/create">Add new post</a>
+                @endcan
+
             </div>
-            <a href="/profile/{{$user->id}}/edit">Edit Profile</a>
+
+            @can('update',$user->profile)
+                <a href="/profile/{{$user->id}}/edit">Edit Profile</a>
+            @endcan
             <div class="d-flex">
                 <div class="pr-5"><strong>{{$user->posts->count()}}</strong> posts</div>
                 <div class="pr-5"> <strong> 23k</strong> followers</div>
