8f81788a96865284cbff680e4c207aa9930fe9b2
AAAAAAAHSAN <bangladeshahsan@gmail.com>
2020-09-17 11:35:56 +0600

    added following system

--- app/Profile.php
+++ app/Profile.php
@@ -13,6 +13,11 @@ class  Profile extends Model
         $imagePath = ($this->image)? $this->image: 'profile/5yhJN6S93Yq7n479Pg1hal4RQlIchvYM6LeBQgva.jpeg';
         return '/storage/' . $imagePath;
     }
+
+    public function followers()
+    {
+        return $this->belongsToMany(User::class);
+    }
     public function user()
     {
         return $this->belongsTo(User::class);
