5d6dd57915cef302daabac2c932a67ac162e25d9
AAAAAAAHSAN <bangladeshahsan@gmail.com>
2020-10-13 21:44:08 +0600

    changing little bit background color

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+<div class="container" style="background-color: #dddddd">
 
     @foreach($posts as $post)
         <div class="row">
